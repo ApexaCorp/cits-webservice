e1786dfe0c423409ac0286273315ea4856677715
Mike Brown <mike.brown0@gmail.com>
2015-07-28 09:51:17 -0400

    Add test harness and documentation for DRAFT specification.

---
+++ CITS-WebService/Apexa.CITS.WCF.Test/GetContractor.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Security;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Apexa.CITS.WCF.Test.CITSService;
+
+namespace Apexa.CITS.WCF.Test
+{
+	[TestClass]
+	public class GetContractor
+	{
+		[TestInitialize]
+		public void Initialize() {
+		}
+
+		[TestMethod]
+		public async Task RequestById()
+		{
+			var client = new CITSService.CITSServiceClient();
+			client.ClientCredentials.UserName.UserName = "cits.carrier@bluesun.ca";
+			client.ClientCredentials.UserName.Password = "password";
+			var requestMessage = new TXLife_Type() { 
+				Version = "2.34.00",
+				Items = new object[] {
+					new TXLifeRequest_Type() {
+						TransRefGUID = Guid.NewGuid().ToString(),
+						TransType = new OLI_LU_TRANS_TYPE_CODES() { tc = "228", Value = "Producer Inquiry" },
+						OLifE = new OLifE_Type() {
+							Items = new object[] {
+								new Party_Type() {
+									id = "C1",
+								}
+							}
+						},
+					}
+				},
+			};
+			var responseMessage = await client.ProcessMessageAsync(requestMessage);
+
+			/* Serialize */
+			CITSHelper.SerializeMessage(requestMessage);
+			CITSHelper.SerializeMessage(responseMessage);
+		}
+
+		[TestMethod]
+		public async Task RequestBySellingCode()
+		{
+			var client = new CITSService.CITSServiceClient();
+			client.ClientCredentials.UserName.UserName = "cits.carrier@bluesun.ca";
+			client.ClientCredentials.UserName.Password = "password";
+			var requestMessage = new TXLife_Type()
+			{
+				Version = "2.34.00",
+				Items = new object[] {
+					new TXLifeRequest_Type() {
+						TransRefGUID = Guid.NewGuid().ToString(),
+						TransType = new OLI_LU_TRANS_TYPE_CODES() { tc = "228", Value = "Producer Inquiry" },
+						OLifE = new OLifE_Type() {
+							Items = new object[] {
+								new Party_Type() {
+									Producer =  new Producer_Type() {
+										CarrierAppointment = new CarrierAppointment_Type[] {
+											new CarrierAppointment_Type() {
+												CompanyProducerID = "12345"
+											}
+										}
+									}
+								}
+							}
+						},
+					}
+				},
+			};
+			var responseMessage = await client.ProcessMessageAsync(requestMessage);
+
+			/* Serialize */
+			CITSHelper.SerializeMessage(requestMessage);
+			CITSHelper.SerializeMessage(responseMessage);
+		}
+
+
+		[TestMethod]
+		public async Task RequestUpdatedContractors()
+		{
+			var client = new CITSService.CITSServiceClient();
+			client.ClientCredentials.UserName.UserName = "cits.carrier@bluesun.ca";
+			client.ClientCredentials.UserName.Password = "password";
+			var requestMessage = new TXLife_Type()
+			{
+				Version = "2.34.00",
+				Items = new object[] {
+					new TXLifeRequest_Type() {
+						TransRefGUID = Guid.NewGuid().ToString(),
+						StartDate = DateTime.Now.Date.AddDays(-7),
+						StartDateSpecified = true,
+						EndDate = DateTime.Now.Date,
+						EndDateSpecified = true,
+						TransType = new OLI_LU_TRANS_TYPE_CODES() { tc = "228", Value = "Producer Inquiry" },
+						TransSubType = new TRANS_SUBTYPE_CODES() { tc = "22817", Value = "Changed Producer Information Only" },
+					}
+				},
+			};
+			var responseMessage = await client.ProcessMessageAsync(requestMessage);
+
+			/* Serialize */
+			CITSHelper.SerializeMessage(requestMessage);
+			CITSHelper.SerializeMessage(responseMessage);
+		}
+
+
+		[TestMethod]
+		public async Task UnauthorizedRequest()
+		{
+			var client = new CITSService.CITSServiceClient();
+			client.ClientCredentials.UserName.UserName = "carrier@bluesun.ca";
+			client.ClientCredentials.UserName.Password = "password";
+			var requestMessage = new TXLife_Type()
+			{
+				Version = "2.34.00",
+				Items = new object[] {
+					new TXLifeRequest_Type() {
+						TransRefGUID = Guid.NewGuid().ToString(),
+						StartDate = DateTime.Now.Date.AddDays(-7),
+						StartDateSpecified = true,
+						EndDate = DateTime.Now.Date,
+						EndDateSpecified = true,
+						TransType = new OLI_LU_TRANS_TYPE_CODES() { tc = "228", Value = "Producer Inquiry" },
+						TransSubType = new TRANS_SUBTYPE_CODES() { tc = "22817", Value = "Changed Producer Information Only" },
+					}
+				},
+			};
+			TXLife_Type result = null;
+			try
+			{
+				result = await client.ProcessMessageAsync(requestMessage);
+				Assert.Fail("Should have failed with unauthorized.");
+			}
+			catch (SecurityAccessDeniedException ex)
+			{
+				Assert.IsTrue(String.Equals(ex.Message, "Access is denied."));
+			}
+		}
+	}
+}
---
+++ CITS-WebService/Apexa.CITS.WCF.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Apexa.CITS.WCF.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Apexa.CITS.WCF.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef7abd9d-0322-456b-9bd2-e9de7f4d74f4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
